677a6361e663d50dad7f4f2028e1979fa4457204
Samir Mulani <mulanisam55@gmail.com>
2021-06-04 19:38:23 +0530

    something has chnaged

--- demo1.java
+++ demo1.java
@@ -1,6 +1,7 @@
 public class demo1 {
     
     public static void main(String[] args) {
-        System.out.println("Hello  Shao");
+        System.out.println("Hello");
+        System.out.println("Hello shaheen");
        }
 }
