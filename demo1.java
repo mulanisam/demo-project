93a2f93c83d30c5a40da0d38fed3ed194b98b91f
Samir Mulani <mulanisam55@gmail.com>
2021-06-04 19:48:00 +0530

    shaheen branch

--- demo1.java
+++ demo1.java
@@ -2,6 +2,6 @@ public class demo1 {
     
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("Hello shaheen");
+        System.out.println("shaheen");
        }
 }
